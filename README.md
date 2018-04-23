b4f9d38e2c641a601922eab33e9909c9648e2f97
Denis Romaniuk <denis@hadornag.ch>
2018-04-23 16:08:34 +0300

    Visualization works

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # graphar
 Tool for visualization HAR-files into graphs
+
+# Usage:
+graphar < map.har > map.dot | zgrviewer -f map.dot
--- graphar.php
+++ graphar.php
@@ -1,15 +1,51 @@
 #!/usr/bin/php
 <?php
+
+function base64url_encode($data) { 
+  return rtrim(strtr(base64_encode($data), '+/', '-_'), '='); 
+} 
+
+function hash_url($url){
+	$parsed_url = parse_url($url);
+	return base64url_encode($url);
+}
+function clear_url($url){
+	$parsed_url = parse_url($url);
+	return $parsed_url['host'].$parsed_url['path'];
+}
+
 $stdin = fopen('php://stdin', 'r');
 $stdout = fopen('php://stdout', 'w');
 
 $har_string = "";
+$har_map = [];
+
 do {
 	$row = fgets($stdin);
 	$har_string .= $row;
 } while ($row !== false);
 
 $har_array = json_decode($har_string);
+
+echo "digraph {\nrankdir=LR;\n";
 foreach ($har_array->log->entries as $entry) {
-	print_r($entry->request->url."\n");
+	$url = $entry->request->url;
+
+	if(preg_match('/\.(jpg|woff2?|webp|png)$/', $url, $matches)) continue;
+
+	$hash = hash_url($url);
+	// echo "\t".$hash.'[label=<<pre>"'.$url.'"</pre>> shape=rectangle]'.";\n";
+	echo "\t".$hash.'[label="'.clear_url($url).'"]'.";\n";
+
+	$referer = false;
+	foreach ($entry->request->headers as $header) {
+		if(strcasecmp($header->name,'Referer')==0){
+			$referer = $header->value;
+		}
+	}
+	if($referer){
+		$referer_hash = hash_url($referer);
+		echo "\t".$referer_hash.' -> '.$hash.";\n";
+	}
 }
+echo "}";
